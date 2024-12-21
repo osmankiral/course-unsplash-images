2d2ceb82ccc936d420e9958a66baba8bd069372a
osmankiral <osman.kiral96@gmail.com>
2024-12-21 12:40:31 +0300

    project completed

---
+++ src/context.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({children}) => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('cat')
+    const toggleDarkTheme = () => {
+        const newDarkTheme = !isDarkTheme
+        setIsDarkTheme(newDarkTheme)
+        const body = document.querySelector('body')
+        body.classList.toggle('dark-theme', newDarkTheme)
+        
+    }
+
+    return(
+        <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>{children}</AppContext.Provider>
+    )
+}
+// qaQKOgoXYfsIDsFq3f0l6ipxfsn0Gt-8pYXTDgYmWjw
+export const useGlobalContext = () => useContext(AppContext)
